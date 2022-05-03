4a580d65e5d3bb06ed1e1016ed3266b6a06ce219
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-05-03 11:59:05 +0530

    new files added

---
+++ Java/com/example/Arrays/one_D_array.java
@@ -0,0 +1,24 @@
+package com.example.Arrays;
+import java.util.*;
+
+public class one_D_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the size of the array : ");
+        n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter the array elements : ");
+        for(int i=0 ; i<n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        // Printing the array elements...
+
+        for(int i:arr){
+            System.out.println(i);
+        }
+
+    }
+}
