c89c754f45c4591b0db37c7e52ca728d2f763b8f
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-05-05 21:45:12 +0530

    new program added

---
+++ Java/com/example/basic_programs/isPalindrome_2nd_approach.java
@@ -0,0 +1,29 @@
+package com.example.basic_programs;
+import java.util.*;
+
+public class isPalindrome_2nd_approach {
+
+    public static boolean isPalindrome(String s){
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start<=end){
+            if(s.charAt(start)==s.charAt(end)){
+                start++;
+                end--;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        System.out.println(isPalindrome(String.valueOf(n)));
+    }
+}
