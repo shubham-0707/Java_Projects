75318794884fdba125b421efd094e7e303c6332c
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-05-11 22:42:35 +0530

    new program added

---
+++ Java/com/example/Recursion/sum_of_number_digits.java
@@ -0,0 +1,25 @@
+package com.example.Recursion;
+import java.util.*;
+
+
+// This program is made to calculate the sum of the digits of a number ....
+public class sum_of_number_digits {
+
+    public static int sumOfDigits(int n){
+        if(n==0){
+            return 0;
+        }
+        return n%10+sumOfDigits(n/10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the number : ");
+        n = sc.nextInt();
+
+        System.out.println("The sum of the digits of the number is : "+sumOfDigits(n));
+
+
+    }
+}
