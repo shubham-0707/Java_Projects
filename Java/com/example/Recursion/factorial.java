1dc11957a99b40554b7e608e3db863b323dec65f
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-05-12 15:58:26 +0530

    new program added

---
+++ Java/com/example/Recursion/factorial.java
@@ -0,0 +1,24 @@
+package com.example.Recursion;
+import java.util.*;
+
+
+// program to calculate the factorial of a number using recursion....
+
+
+public class factorial {
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the n : ");
+        n = sc.nextInt();
+
+        System.out.println("The factorial of "+n+" is "+fact(n));
+    }
+}
