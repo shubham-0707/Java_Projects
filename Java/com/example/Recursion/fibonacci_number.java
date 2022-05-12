3ef314627d243cc4ea6d33c56f0446610d88b2cb
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-05-12 16:03:45 +0530

    new program added

---
+++ Java/com/example/Recursion/fibonacci_number.java
@@ -0,0 +1,23 @@
+package com.example.Recursion;
+import java.util.*;
+
+// Program to find the nth fibonacci number....
+public class fibonacci_number {
+
+    public static int fibo(int n){
+        if(n==1 || n==2){
+            return 1;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the number : ");
+        n = sc.nextInt();
+
+        System.out.println("The "+n+"th"+" fibonacci number is "+fibo(n));
+
+    }
+}
