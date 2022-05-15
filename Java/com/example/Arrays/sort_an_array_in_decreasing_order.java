61656793c74de9409d3c497a811e3f828a0b6e3b
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-05-15 10:38:06 +0530

    new program added

---
+++ Java/com/example/Arrays/sort_an_array_in_decreasing_order.java
@@ -0,0 +1,45 @@
+package com.example.Arrays;
+import java.util.*;
+
+
+public class sort_an_array_in_decreasing_order {
+
+    public static void reverse(int[] arr){
+        int start=0;
+        int end = arr.length -1 ;
+
+        while(start<=end){
+            int temp = arr[start];
+            arr[start]= arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter n : ");
+        n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements : ");
+
+        for(int i=0 ; i<n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        reverse(arr);
+
+        System.out.println("The sorted array in decreasing order is : ");
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+
+
+    }
+
+}
