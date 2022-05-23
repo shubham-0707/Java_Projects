51163abfbdaea3f73932a9d4abcdd9329abe04d7
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-05-23 19:46:56 +0530

    new program added

---
+++ Java/com/example/Sorting/Bubble_Sort.java
@@ -0,0 +1,37 @@
+package com.example.Sorting;
+import java.util.*;
+
+// Program to implement bubble sort in array...
+public class Bubble_Sort {
+    public static void main(String[] args) {
+        System.out.println("Enter n : ");
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the array elements : ");
+        for(int i=0 ; i<n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        // Bubble Sort process begins here..
+
+        for(int i=0 ; i<n ; i++){
+            for(int j=i+1 ; j<n ; j++){
+                if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("The sorted array is : ");
+        for(int i=0 ; i<n ; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+
+    }
+}
