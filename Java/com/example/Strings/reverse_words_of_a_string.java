689c313c11b6f97980b6c7776fde6386ce1e1503
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-05-26 13:46:48 +0530

    new program added

---
+++ Java/com/example/Strings/reverse_words_of_a_string.java
@@ -0,0 +1,23 @@
+package com.example.Strings;
+import java.util.*;
+
+public class reverse_words_of_a_string {
+    public static void main(String[] args) {
+        String s = "a good   example";
+
+        String[] str = s.split(" ");
+
+        String ans = "";
+        for(int i=str.length-1 ; i>=0 ; i--){
+            if(str[i].isEmpty()==false){
+                ans = ans + str[i] + " ";
+            }
+        }
+
+
+        System.out.println("The reversed array with removed extra spaces is : ");
+
+        System.out.println(ans);
+
+    }
+}
