5b9680adef3370e225d4a2f20ecfecef74cca334
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-05-27 00:31:05 +0530

    new program added

--- Java/com/example/Searching/Binary_Search_questions/search_in_a_mountain_array.java
+++ Java/com/example/Searching/Binary_Search_questions/search_in_a_mountain_array.java
@@ -1,35 +1,32 @@
 package com.example.Searching.Binary_Search_questions;
 import java.util.*;
 
+
+// Program to find an element's index in a mountain array using binary search...
 public class search_in_a_mountain_array {
 
-
-    // Program to search element index in a mountain array using binary search..
     public static int findPeak(int[] arr){
         int start = 0;
         int end = arr.length-1;
         int peak = -1;
 
         while(start<end){
-            int mid = start + (end - start)/2;
-
+            int mid = start + (end-start)/2;
             if(arr[mid]<arr[mid+1]){
+                peak = start+1;
                 start = mid+1;
-                peak = mid+1;
             }
-            else if(arr[mid]>arr[mid+1]){
-                end = mid;
+            else{
                 peak = mid;
+                end = mid;
             }
         }
-
         return peak;
     }
 
-
     public static int BinarySearch(int[] arr , int start , int end , int key){
         while(start<=end){
-            int mid = start+(end-start)/2;
+            int mid = start + (end-start)/2;
 
             if(arr[mid]==key){
                 return mid;
@@ -38,40 +35,56 @@ public class search_in_a_mountain_array {
                 start = mid+1;
             }
             else{
-                end = mid-1;
+                end = key-1;
             }
         }
+        return -1;
+    }
 
+    public static int revBinarySearch(int[] arr , int start , int end , int key){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]==key){
+                return mid;
+            }
+            else if(arr[mid]<key){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
         return -1;
     }
 
 
     public static void main(String[] args) {
-        System.out.println("Enter n : ");
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n : ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-        System.out.println("Enter the array elements : ");
+        System.out.println("Enter n array elements : ");
         for(int i=0 ; i<n ; i++){
             arr[i] = sc.nextInt();
         }
 
         System.out.println("Enter the search element : ");
-        int B = sc.nextInt();
+        int key = sc.nextInt();
 
-        int peak =findPeak(arr);
+        int peak = findPeak(arr);
 
-
-        if(B == arr[peak]){
+        if(arr[peak]==key){
             System.out.println("Index : "+peak);
         }
+        else{
+            int res = BinarySearch(arr , 0 , peak-1 , key);
 
-        else if(B<arr[peak]){
-            System.out.println("Index : "+BinarySearch(arr , 0 , peak-1 , B));
+            if(res==-1){
+                res = revBinarySearch(arr , peak+1 , arr.length-1 , key);
+            }
+            System.out.println("Index : "+res);
         }
-        else {
-            System.out.println("Index : " + BinarySearch(arr, peak + 1, arr.length - 1, B));
-        }
+
     }
 }
