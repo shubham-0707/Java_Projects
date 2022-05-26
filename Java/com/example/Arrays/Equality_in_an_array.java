aeb8250d84201fbec9d0e6a74ddb55a3e0dc6059
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-05-26 14:14:21 +0530

    new program added

---
+++ Java/com/example/Arrays/Equality_in_an_array.java
@@ -0,0 +1,26 @@
+package com.example.Arrays;
+import java.util.*;
+
+// Program to check that how many elements are there which have occurred once in the array...
+public class Equality_in_an_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] count = new int[101];
+        int[] arr = new int[n];
+        for(int i=0 ; i<n ; i++){
+            arr[i] = sc.nextInt();
+            count[arr[i]]++;
+        }
+
+        int maxi = Integer.MIN_VALUE;
+
+        for(int i=0 ; i<101 ; i++){
+            maxi = Math.max(maxi , count[i]);
+        }
+
+        int cnt = n-maxi;
+
+        System.out.println("The minimum number of elements to be removed : "+cnt);
+    }
+}
