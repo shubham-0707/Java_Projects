149da6fceeba0baec677e6d04d5c13d4063db5f8
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-06-24 14:45:30 +0530

    new program added

---
+++ Java/com/example/OOPs/Doctor.java
@@ -0,0 +1,43 @@
+package com.example.OOPs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Doctor {
+    private String Doctor;
+    private List<Patient> PatientList;
+
+    public void __init__(List<Patient> PatientList){
+        this.PatientList = PatientList;
+    }
+
+    public Patient findPatientWithMaximumAge(){
+        if(PatientList.isEmpty()){
+            System.out.println("No Data Found");
+        }
+        int maxi = 0;
+        int pointer=0;
+        int x = 0;
+        for(Patient p : PatientList){
+            if(p.age>maxi){
+                maxi=p.age;
+                pointer=x;
+            }
+            x++;
+        }
+        return PatientList.get(pointer);
+    }
+
+    public ArrayList<Double> sortPatientByBillAmount(){
+        if(PatientList.isEmpty()){
+            System.out.println("No Data Found");
+        }
+        ArrayList<Double> arr = new ArrayList<Double>();
+        for(Patient p : PatientList){
+            arr.add(p.billAmount);
+        }
+        Collections.sort(arr);
+        return arr;
+    }
+}
---
+++ Java/com/example/OOPs/Test.java
@@ -0,0 +1,43 @@
+package com.example.OOPs;
+import java.util.*;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+
+        List<Patient> patient = new ArrayList<>();
+        for(int i=0 ; i<n ; i++){
+            Patient p = new Patient();
+            String Code = sc.next();
+            String Name = sc.next();
+            int age = sc.nextInt();
+            String doctorName = sc.next();
+            double billAmount = sc.nextDouble();
+
+            p.__init__(Code , Name , age , doctorName , billAmount);
+
+            patient.add(p);
+        }
+
+        Doctor d = new Doctor();
+        d.__init__(patient);
+
+        Patient maximumAge = new Patient();
+        maximumAge = d.findPatientWithMaximumAge();
+        System.out.println(maximumAge.Code);
+        System.out.println(maximumAge.Name);
+        System.out.println(maximumAge.age);
+        System.out.println(maximumAge.doctorName);
+        System.out.println(maximumAge.billAmount);
+
+        ArrayList<Double> ans = new ArrayList<Double>();
+
+        ans = d.sortPatientByBillAmount();
+        for(int i=0 ; i<ans.size() ; i++){
+            System.out.println(ans.get(i));
+        }
+    }
+}
