c5e217c1bc4d777bc58421f696cdc3399edf97fe
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-07-07 01:32:13 +0530

    Find the next greater element in the array...

---
+++ Java/com/example/Stack/Next_Greater_Element.java
@@ -0,0 +1,43 @@
+package com.example.Stack;
+import java.util.*;
+
+
+// This program is made to find out the next greater element in the right of the element in the array...
+public class Next_Greater_Element {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0 ; i<n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Stack<Integer> st = new Stack<>();
+        Stack<Integer> ans = new Stack<>();
+        for(int i=n-1 ; i>=0 ; i--){
+            if(st.isEmpty()){
+                st.push(arr[i]);
+                ans.push(-1);
+            }
+            else{
+                while(!st.isEmpty() && st.peek()<=arr[i]){
+                    st.pop();
+                }
+                if(!st.isEmpty()){
+                    ans.push(st.peek());
+                }
+                else {
+                    ans.push(-1);
+                }
+                st.push(arr[i]);
+            }
+        }
+
+
+        while(!ans.isEmpty()){
+            System.out.print(ans.pop()+" ");
+        }
+    }
+}
