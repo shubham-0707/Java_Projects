103c51a1e7e81c8a2ed7f34866b249ad82bebee1
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-07-09 00:15:18 +0530

    Next Smaller element in an array...

---
+++ Java/com/example/Stack/next_smaller_element.java
@@ -0,0 +1,46 @@
+package com.example.Stack;
+import java.util.*;
+
+
+// This program is made to get the next smaller element in an array of an array element...
+public class next_smaller_element {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i=0 ; i<n ; i++){
+            arr[i]= sc.nextInt();
+        }
+
+        Stack<Integer> st = new Stack<>();
+        Stack<Integer> ans = new Stack<>();
+
+        for(int i=n-1 ; i>=0 ; i--){
+            if(st.isEmpty()){
+                st.push(arr[i]);
+                ans.push(-1);
+            }
+            else{
+                while(!st.isEmpty() && st.peek()>=arr[i]){
+                    st.pop();
+                }
+
+                if(st.isEmpty()){
+                    ans.push(-1);
+                }
+                else{
+                    ans.push(st.peek());
+                }
+
+                st.push(arr[i]);
+            }
+        }
+
+
+        while(!ans.isEmpty()){
+            System.out.print(ans.pop()+" ");
+        }
+
+    }
+}
