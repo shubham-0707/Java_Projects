0ef1243bbce2156fc1d9f45c5c558dbf7adf914a
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-07-13 17:28:21 +0530

    Some more questions on recursion..

---
+++ Java/com/example/Recursion/palindrome_in_a_string.java
@@ -0,0 +1,18 @@
+package com.example.Recursion;
+import java.util.*;
+
+// This program is just made to check that a string is a palindrome or not...
+public class palindrome_in_a_string {
+    public static String checkPalindrome(String str){
+
+        if(str.length()==0){
+            return "";
+        }
+        return str.charAt(str.length()-1) + checkPalindrome(str.substring(0 , str.length()-1));
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str.equals(checkPalindrome(str)));
+    }
+}
---
+++ Java/com/example/Recursion/print_name_n_times.java
@@ -0,0 +1,25 @@
+package com.example.Recursion;
+import java.util.*;
+
+
+// This program is made to print the name of the user n times using recursion...
+public class print_name_n_times {
+
+    public static void print(int i , int n  , String name){
+        if(i>n){
+            return;
+        }
+        System.out.println(name);
+        print(i+1 , n , name);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n : ");
+        int n = sc.nextInt();
+        System.out.println("Enter String : ");
+        String str = sc.next();
+
+        print(1 , n , str);
+    }
+
+}
