a4b0f792769ca506bfe0be0915f88768cc5cf191
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-07-13 18:22:47 +0530

    Some more questions on recursion..

--- Java/com/example/Recursion/fibonacci_number.java
+++ Java/com/example/Recursion/fibonacci_number.java
@@ -1,23 +1,24 @@
 package com.example.Recursion;
 import java.util.*;
 
-// Program to find the nth fibonacci number....
+// Program to find the nth fibonacci number using recursion....
 public class fibonacci_number {
 
     public static int fibo(int n){
-        if(n==1 || n==2){
+        if(n==1){
+            return 0;
+        }
+        else if(n==2){
             return 1;
         }
-        return fibo(n-1)+fibo(n-2);
-    }
 
+        return fibo(n-1) + fibo(n-2);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n;
-        System.out.println("Enter the number : ");
-        n = sc.nextInt();
+        System.out.println("Enter n : ");
+        int n = sc.nextInt();
 
-        System.out.println("The "+n+"th"+" fibonacci number is "+fibo(n));
-
+        System.out.println("The desired fibonacci number is : "+fibo(n));
     }
 }
