ed377cdcbcb7eb888edd396a1d87efc8914656cf
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-07-18 18:36:08 +0530

    Tree traversals of DFS...

---
+++ Java/com/example/Tree/Inorder.java
@@ -0,0 +1,44 @@
+package com.example.Tree;
+
+
+// This program is written to find the inorder traversal of a tree. And inorder traversal is a part of DFS...
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        this.left=null;
+        this.right = null;
+    }
+}
+
+public class Inorder {
+    public static void inorder(Node root){
+        if(root==null) return;
+
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(10);
+        Node left1 = new Node(9);
+        Node right1 = new Node(8);
+        root.left = left1;
+        root.right = right1;
+        Node left2 = new Node(7);
+        left1.left = left2;
+        Node left3 = new Node(6);
+        left2.right = left3;
+        Node right2 = new Node(5);
+        right1.left = right2;
+        Node right3 = new Node(12);
+        right1.right = right3;
+        System.out.println("Inorder : ");
+        inorder(root);
+    }
+}
---
+++ Java/com/example/Tree/Postorder.java
@@ -0,0 +1,35 @@
+package com.example.Tree;
+
+
+// This program is made to find the postorder of an binary tree...
+// Since Node class is already implemented in the Inorder class...
+// That's why we don't need to implement it again..
+public class Postorder {
+
+    public static void postorder(Node root){
+        if(root==null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data+" ");
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        Node left1 = new Node(9);
+        Node right1 = new Node(8);
+        root.left = left1;
+        root.right = right1;
+        Node left2 = new Node(7);
+        left1.left = left2;
+        Node left3 = new Node(6);
+        left2.right = left3;
+        Node right2 = new Node(5);
+        right1.left = right2;
+        Node right3 = new Node(12);
+        right1.right = right3;
+        System.out.println("PostOrder : ");
+        postorder(root);
+    }
+}
